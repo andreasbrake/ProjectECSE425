92cef0faccfeb6bffdd2369330a9ed3184c4ff21
andreasbrake <andreas@andreasbrake.ca>
2015-02-20 00:08:11 -0500

    removed tabs

--- Assembler/dictionary.c
+++ Assembler/dictionary.c
@@ -16,8 +16,8 @@
 #define NUM_OF_J 2
 
 typedef struct {
-	char** label;
-	int line;
+    char** label;
+    int line;
 } Label;
 
 typedef struct {
@@ -39,225 +39,225 @@ int pairsI = 0; // ..
 int pairsJ = 0; // ..
 
 int convert_instruction(char* instruction, char** opcode, char** function){
-	int i;
-	for(i=0; i < NUM_OF_R; i++){
-		if(!strcmp(opcodesR[i].key, instruction)){
-			*opcode = opcodesR[i].opcode;
-			*function = opcodesR[i].function;
-			return 0;
-		}
-	}
-	for(i=0; i < NUM_OF_I; i++){
-		if(!strcmp(opcodesI[i].key, instruction)){
-			*opcode = opcodesI[i].opcode;
-			return 1;
-		}
-	}
-	for(i=0; i < NUM_OF_J; i++){
-		if(!strcmp(opcodesJ[i].key, instruction)){
-			*opcode = opcodesJ[i].opcode;
-			return 2;
-		}
-	}
+    int i;
+    for(i=0; i < NUM_OF_R; i++){
+        if(!strcmp(opcodesR[i].key, instruction)){
+            *opcode = opcodesR[i].opcode;
+            *function = opcodesR[i].function;
+            return 0;
+        }
+    }
+    for(i=0; i < NUM_OF_I; i++){
+        if(!strcmp(opcodesI[i].key, instruction)){
+            *opcode = opcodesI[i].opcode;
+            return 1;
+        }
+    }
+    for(i=0; i < NUM_OF_J; i++){
+        if(!strcmp(opcodesJ[i].key, instruction)){
+            *opcode = opcodesJ[i].opcode;
+            return 2;
+        }
+    }
 
-	printf("looking for %s", instruction);
-	return -1;
+    printf("looking for %s", instruction);
+    return -1;
 }
 int convert_to_binary(int linenum, char* numchar, int length, char** bin){
-	int num = 0;
-	int res = sscanf(numchar, "%d", &num);
+    int num = 0;
+    int res = sscanf(numchar, "%d", &num);
 
-	if(res == 0){
-		int i;
-		for(i=0; i < NUM_OF_R; i++){
-			if(!strcmp((char*)&labels[i].label, numchar)){
-				num = labels[i].line - linenum;
-				if(num > 0)
-					num--;
-				break;
-			}
-		}
-	}else{
-		num = atoi(numchar);
-	}
+    if(res == 0){
+        int i;
+        for(i=0; i < NUM_OF_R; i++){
+            if(!strcmp((char*)&labels[i].label, numchar)){
+                num = labels[i].line - linenum;
+                if(num > 0)
+                    num--;
+                break;
+            }
+        }
+    }else{
+        num = atoi(numchar);
+    }
 
-	char binNum[length];
-	binNum[0] = '\0';
+    char binNum[length];
+    binNum[0] = '\0';
 
-	int i;
-	for(i=(length-1); i>=0; i--){
-		binNum[i] = (num & 1) + '0';
-		num >>= 1;
-	}
+    int i;
+    for(i=(length-1); i>=0; i--){
+        binNum[i] = (num & 1) + '0';
+        num >>= 1;
+    }
 
-	binNum[length] = '\0';
+    binNum[length] = '\0';
 
-	char* tempbin;
-	tempbin = (char*)malloc(length);
-	tempbin[0] = '\0';
-	strcpy(tempbin, binNum);
+    char* tempbin;
+    tempbin = (char*)malloc(length);
+    tempbin[0] = '\0';
+    strcpy(tempbin, binNum);
 
-	*bin = tempbin;
+    *bin = tempbin;
 
-	return 0;
+    return 0;
 }
 
 int add_label(char** label, int lineNumber){
-	Label newLabel;
-	newLabel.label = (char**)*label;
-	newLabel.line = lineNumber;
+    Label newLabel;
+    newLabel.label = (char**)*label;
+    newLabel.line = lineNumber;
 
-	labels = (Label*)realloc(labels, (lblnum+1) * sizeof(newLabel));
-	labels[lblnum] = newLabel;
+    labels = (Label*)realloc(labels, (lblnum+1) * sizeof(newLabel));
+    labels[lblnum] = newLabel;
 
-	lblnum ++;
-	return 0;
+    lblnum ++;
+    return 0;
 }
 int add_key_val(char* key, char* opcode, char* type){
-	Opfun op;
-	op.key = key;
-	op.opcode = opcode;
-	op.function = "";
+    Opfun op;
+    op.key = key;
+    op.opcode = opcode;
+    op.function = "";
 
-	if(!strcmp(type, "R1")){
-		op.function = opcode;
-		op.opcode = "000000";
-		opcodesR[pairsR] = op;
-		pairsR++;
-	}
-	if(!strcmp(type, "R2")){
-		op.function = opcode;
-		op.opcode = "010001";
-		opcodesR[pairsR] = op;
-		pairsR++;
-	}
-	else if(!strcmp(type, "I")){
-		opcodesI[pairsI] = op;
-		pairsI++;
-	}
-	else if(!strcmp(type, "J")){
-		opcodesJ[pairsJ] = op;
-		pairsJ++;
-	}
+    if(!strcmp(type, "R1")){
+        op.function = opcode;
+        op.opcode = "000000";
+        opcodesR[pairsR] = op;
+        pairsR++;
+    }
+    if(!strcmp(type, "R2")){
+        op.function = opcode;
+        op.opcode = "010001";
+        opcodesR[pairsR] = op;
+        pairsR++;
+    }
+    else if(!strcmp(type, "I")){
+        opcodesI[pairsI] = op;
+        pairsI++;
+    }
+    else if(!strcmp(type, "J")){
+        opcodesJ[pairsJ] = op;
+        pairsJ++;
+    }
 
-	return 0;
+    return 0;
 }
 
 int init_R1_codes(){
-	add_key_val("sll",  "000000", "R1");
-	add_key_val("srl",  "000010", "R1");
-	add_key_val("sra",  "000011", "R1");
+    add_key_val("sll",  "000000", "R1");
+    add_key_val("srl",  "000010", "R1");
+    add_key_val("sra",  "000011", "R1");
 
-	add_key_val("sllv", "000100", "R1");
-	add_key_val("srlv", "000110", "R1");
-	add_key_val("srav", "000111", "R1");
+    add_key_val("sllv", "000100", "R1");
+    add_key_val("srlv", "000110", "R1");
+    add_key_val("srav", "000111", "R1");
 
-	add_key_val("jr",   "001000", "R1");
-	add_key_val("jalr", "001001", "R1");
-	add_key_val("movz", "001010", "R1");
-	add_key_val("movn", "001011", "R1");
+    add_key_val("jr",   "001000", "R1");
+    add_key_val("jalr", "001001", "R1");
+    add_key_val("movz", "001010", "R1");
+    add_key_val("movn", "001011", "R1");
 
-	add_key_val("syscall", "001100", "R1");
-	add_key_val("break",   "001101", "R1");
-	add_key_val("sync",    "001111", "R1");
+    add_key_val("syscall", "001100", "R1");
+    add_key_val("break",   "001101", "R1");
+    add_key_val("sync",    "001111", "R1");
 
-	add_key_val("mfhi", "010000", "R1");
-	add_key_val("mthi", "010001", "R1");
-	add_key_val("mflo", "010010", "R1");
-	add_key_val("mtlo", "010011", "R1");
+    add_key_val("mfhi", "010000", "R1");
+    add_key_val("mthi", "010001", "R1");
+    add_key_val("mflo", "010010", "R1");
+    add_key_val("mtlo", "010011", "R1");
 
-	add_key_val("mult", "011000", "R1");
-	add_key_val("multu","011001", "R1");
-	add_key_val("div",  "011010", "R1");
-	add_key_val("divu", "011011", "R1");
+    add_key_val("mult", "011000", "R1");
+    add_key_val("multu","011001", "R1");
+    add_key_val("div",  "011010", "R1");
+    add_key_val("divu", "011011", "R1");
 
-	add_key_val("add",  "100000", "R1");
-	add_key_val("addu", "100001", "R1");
-	add_key_val("sub",  "100010", "R1");
-	add_key_val("subu", "100011", "R1");
+    add_key_val("add",  "100000", "R1");
+    add_key_val("addu", "100001", "R1");
+    add_key_val("sub",  "100010", "R1");
+    add_key_val("subu", "100011", "R1");
 
-	add_key_val("and",  "100100", "R1");
-	add_key_val("or",   "100101", "R1");
-	add_key_val("xor",  "100110", "R1");
-	add_key_val("nor",  "100111", "R1");
+    add_key_val("and",  "100100", "R1");
+    add_key_val("or",   "100101", "R1");
+    add_key_val("xor",  "100110", "R1");
+    add_key_val("nor",  "100111", "R1");
 
-	add_key_val("slt",  "101010", "R1");
-	add_key_val("sltu", "101011", "R1");
+    add_key_val("slt",  "101010", "R1");
+    add_key_val("sltu", "101011", "R1");
 
-	add_key_val("tge",  "110000", "R1");
-	add_key_val("tgeu", "110001", "R1");
-	add_key_val("tlt",  "110010", "R1");
-	add_key_val("tltu", "110011", "R1");
+    add_key_val("tge",  "110000", "R1");
+    add_key_val("tgeu", "110001", "R1");
+    add_key_val("tlt",  "110010", "R1");
+    add_key_val("tltu", "110011", "R1");
 
-	add_key_val("teq",  "110100", "R1");
-	add_key_val("tne",  "110110", "R1");
+    add_key_val("teq",  "110100", "R1");
+    add_key_val("tne",  "110110", "R1");
 
-	return 0;
+    return 0;
 }
 
 int init_I_codes(){
-	add_key_val("beq",  "000100", "I");
-	add_key_val("bne",  "000101", "I");
-	add_key_val("blez", "000110", "I");
-	add_key_val("bgtz", "000111", "I");
+    add_key_val("beq",  "000100", "I");
+    add_key_val("bne",  "000101", "I");
+    add_key_val("blez", "000110", "I");
+    add_key_val("bgtz", "000111", "I");
 
-	add_key_val("addi", "001000", "I");
-	add_key_val("addiu","001001", "I");
-	add_key_val("slti", "001010", "I");
-	add_key_val("sltiu","001011", "I");
+    add_key_val("addi", "001000", "I");
+    add_key_val("addiu","001001", "I");
+    add_key_val("slti", "001010", "I");
+    add_key_val("sltiu","001011", "I");
 
-	add_key_val("andi", "001100", "I");
-	add_key_val("ori",  "001101", "I");
-	add_key_val("xori", "001110", "I");
-	add_key_val("lui",  "001111", "I");
+    add_key_val("andi", "001100", "I");
+    add_key_val("ori",  "001101", "I");
+    add_key_val("xori", "001110", "I");
+    add_key_val("lui",  "001111", "I");
 
-	add_key_val("lb",   "100000", "I");
-	add_key_val("lh",   "100001", "I");
-	add_key_val("lwl",  "100010", "I");
-	add_key_val("lw",   "100011", "I");
+    add_key_val("lb",   "100000", "I");
+    add_key_val("lh",   "100001", "I");
+    add_key_val("lwl",  "100010", "I");
+    add_key_val("lw",   "100011", "I");
 
-	add_key_val("lbu",  "100100", "I");
-	add_key_val("lhu",  "100101", "I");
-	add_key_val("lwr",  "100110", "I");
+    add_key_val("lbu",  "100100", "I");
+    add_key_val("lhu",  "100101", "I");
+    add_key_val("lwr",  "100110", "I");
 
-	add_key_val("sb",   "101000", "I");
-	add_key_val("sh",   "101001", "I");
-	add_key_val("swl",  "101010", "I");
-	add_key_val("sw",   "101011", "I");
+    add_key_val("sb",   "101000", "I");
+    add_key_val("sh",   "101001", "I");
+    add_key_val("swl",  "101010", "I");
+    add_key_val("sw",   "101011", "I");
 
-	add_key_val("swr",  "101110", "I");
-	add_key_val("cache","101111", "I");
+    add_key_val("swr",  "101110", "I");
+    add_key_val("cache","101111", "I");
 
-	add_key_val("ll",   "110000", "I");
-	add_key_val("lwc1", "110001", "I");
-	add_key_val("lwc2", "110010", "I");
-	add_key_val("pref", "110011", "I");
+    add_key_val("ll",   "110000", "I");
+    add_key_val("lwc1", "110001", "I");
+    add_key_val("lwc2", "110010", "I");
+    add_key_val("pref", "110011", "I");
 
-	add_key_val("ldc1", "110101", "I");
-	add_key_val("ldc2", "110110", "I");
+    add_key_val("ldc1", "110101", "I");
+    add_key_val("ldc2", "110110", "I");
 
-	add_key_val("sc",   "111000", "I");
-	add_key_val("swc1", "111001", "I");
-	add_key_val("swc2", "111010", "I");
+    add_key_val("sc",   "111000", "I");
+    add_key_val("swc1", "111001", "I");
+    add_key_val("swc2", "111010", "I");
 
-	add_key_val("sdc1", "111101", "I");
-	add_key_val("sdc2", "111110", "I");
+    add_key_val("sdc1", "111101", "I");
+    add_key_val("sdc2", "111110", "I");
 
-	return 0;
+    return 0;
 }
 int init_J_codes(){
-	add_key_val("j",    "000010", "J");
-	add_key_val("jal",  "000011", "J");
+    add_key_val("j",    "000010", "J");
+    add_key_val("jal",  "000011", "J");
 
-	return 0;
+    return 0;
 }
 int init_dictionary(){
 
-	init_R1_codes();
-	init_I_codes();
-	init_J_codes();
+    init_R1_codes();
+    init_I_codes();
+    init_J_codes();
 
 
 
-	return 0;
+    return 0;
 }
--- Assembler/main.c
+++ Assembler/main.c
@@ -20,173 +20,173 @@ int pos = 0;
 int wordType = 0;
 
 char* append(char* str1, char* str2){
-	char* temp = "";
-	if((temp = malloc(strlen(str1)+strlen(str2)+1)) != NULL){
-		temp[0] = '\0';
-		strcat(temp,str1);
-		strcat(temp,str2);
-	} else{
-		printf("ERROR APPENDING STRING");
-	}
-	return temp;
+    char* temp = "";
+    if((temp = malloc(strlen(str1)+strlen(str2)+1)) != NULL){
+        temp[0] = '\0';
+        strcat(temp,str1);
+        strcat(temp,str2);
+    } else{
+        printf("ERROR APPENDING STRING");
+    }
+    return temp;
 }
 
 int add_to_word(char* bin){
-	int len;
-	int i;
-	int offset = 0;
+    int len;
+    int i;
+    int offset = 0;
 
-	if(pos == 0){ // opcode (will always be the first thing added independent of instruction type)
-		len = 6;
-		offset = 0;
-	}else{
-		if(wordType == 0){ // R instruction
-			if(pos == 1){ // funct
-				len = 6;
-				offset = 26;
-			}else if(pos == 2){ // rd
-				len = 5;
-				offset = 16;
-			}else if(pos == 3){ // rs
-				len = 5;
-				offset = 6;
-			}else if(pos == 4){ // rt
-				len = 5;
-				offset = 11;
-			}
-		}else if(wordType == 1){ // I instruction
-			if(pos == 1){ // rt
-				len = 5;
-				offset = 11;
-			}else if(pos == 2){ // rs
-				len = 5;
-				offset = 6;
-			}else if(pos == 3){ // immediate
-				len = 16;
-				offset = 16;
-			}
-		}else if(wordType == 2){ // J instruction
-			if(pos == 1){ // address
-				len = 26;
-				offset = 6;
-			}
-		}
-	}
+    if(pos == 0){ // opcode (will always be the first thing added independent of instruction type)
+        len = 6;
+        offset = 0;
+    }else{
+        if(wordType == 0){ // R instruction
+            if(pos == 1){ // funct
+                len = 6;
+                offset = 26;
+            }else if(pos == 2){ // rd
+                len = 5;
+                offset = 16;
+            }else if(pos == 3){ // rs
+                len = 5;
+                offset = 6;
+            }else if(pos == 4){ // rt
+                len = 5;
+                offset = 11;
+            }
+        }else if(wordType == 1){ // I instruction
+            if(pos == 1){ // rt
+                len = 5;
+                offset = 11;
+            }else if(pos == 2){ // rs
+                len = 5;
+                offset = 6;
+            }else if(pos == 3){ // immediate
+                len = 16;
+                offset = 16;
+            }
+        }else if(wordType == 2){ // J instruction
+            if(pos == 1){ // address
+                len = 26;
+                offset = 6;
+            }
+        }
+    }
 
-	for(i=0; i<len; i++){
-		lineWord[(offset+i)] = bin[i];
-	}
+    for(i=0; i<len; i++){
+        lineWord[(offset+i)] = bin[i];
+    }
 
-	pos++;
-	return 0;
+    pos++;
+    return 0;
 }
 
 int parse_line(char input[]){
-	lineWord = (char*)malloc(32);
-	int i;
-	for(i=0; i<32; i++){
-		lineWord[i] = '0';
-	}
-	lineWord[32] = '\0';
-	wordType = 0;
-	pos = 0;
+    lineWord = (char*)malloc(32);
+    int i;
+    for(i=0; i<32; i++){
+        lineWord[i] = '0';
+    }
+    lineWord[32] = '\0';
+    wordType = 0;
+    pos = 0;
 
-	int instructionRead = 0;
-	int readingWord = 0;
+    int instructionRead = 0;
+    int readingWord = 0;
 
-	char inst[32];
-	inst[0] = '\0';
+    char inst[32];
+    inst[0] = '\0';
 
-	for(i=0; i<strlen(input); i++){
-		if(input[i] == '#') // Line is over if comment begins
-			break;
-		else if(input[i] == ':'){ // line flag
-			readingWord = 0;
-			inst[0] = '\0';
-			continue;
-		}
-		else if(input[i] == ','){ // End of register or value
-			char* word = malloc(5);
-			convert_to_binary(lineNumber, inst, 5, &word);
-			add_to_word(word);
+    for(i=0; i<strlen(input); i++){
+        if(input[i] == '#') // Line is over if comment begins
+            break;
+        else if(input[i] == ':'){ // line flag
+            readingWord = 0;
+            inst[0] = '\0';
+            continue;
+        }
+        else if(input[i] == ','){ // End of register or value
+            char* word = malloc(5);
+            convert_to_binary(lineNumber, inst, 5, &word);
+            add_to_word(word);
 
-			readingWord = 0;
-			inst[0] = '\0';
-			continue;
-		}
-		else if(input[i] == ' ' || input[i] == '\t'){ // End of opcode or just whitespace
-			if(readingWord){
-				if(!instructionRead){
-					char* opcode = "";
-					char* function = "";
+            readingWord = 0;
+            inst[0] = '\0';
+            continue;
+        }
+        else if(input[i] == ' ' || input[i] == '\t'){ // End of opcode or just whitespace
+            if(readingWord){
+                if(!instructionRead){
+                    char* opcode = "";
+                    char* function = "";
 
-					wordType = convert_instruction(inst, &opcode, &function);
-					add_to_word(opcode);
-					if(wordType == 0){
-						add_to_word(function);
-					}
+                    wordType = convert_instruction(inst, &opcode, &function);
+                    add_to_word(opcode);
+                    if(wordType == 0){
+                        add_to_word(function);
+                    }
 
-					instructionRead = 1;
-					lineNumber++;
+                    instructionRead = 1;
+                    lineNumber++;
 
-					inst[0] = '\0';
-				}
-				readingWord = 0;
-			}
-			continue;
-		}
-		else if(input[i] == '$'){
-			continue;
-		}
-		else{
-			int len = strlen(inst);
-			inst[len] = input[i];
-			inst[len+1] = '\0';
-			readingWord = 1;
-		}
-	}
-	if(inst[0] != '\0'){
-		char* thing = ""; // either rt, immediate, or address depending on instruction type
-		if(wordType == 0){
-			convert_to_binary(lineNumber, inst, 5, &thing);
-		}else if(wordType == 1){
-			convert_to_binary(lineNumber, inst, 16, &thing);
-		}else if(wordType == 2){
-			convert_to_binary(lineNumber, inst, 26, &thing);
-		}else{
-			printf("ERROR!");
-		}
-		if(strcmp(thing, ""))
-			add_to_word(thing);
-	}
+                    inst[0] = '\0';
+                }
+                readingWord = 0;
+            }
+            continue;
+        }
+        else if(input[i] == '$'){
+            continue;
+        }
+        else{
+            int len = strlen(inst);
+            inst[len] = input[i];
+            inst[len+1] = '\0';
+            readingWord = 1;
+        }
+    }
+    if(inst[0] != '\0'){
+        char* thing = ""; // either rt, immediate, or address depending on instruction type
+        if(wordType == 0){
+            convert_to_binary(lineNumber, inst, 5, &thing);
+        }else if(wordType == 1){
+            convert_to_binary(lineNumber, inst, 16, &thing);
+        }else if(wordType == 2){
+            convert_to_binary(lineNumber, inst, 26, &thing);
+        }else{
+            printf("ERROR!");
+        }
+        if(strcmp(thing, ""))
+            add_to_word(thing);
+    }
 
-	return 0;
+    return 0;
 }
 int parse_line_labels(char input[]){
-	int i;
+    int i;
 
-	char label[32];
-	label[0] = '\0';
+    char label[32];
+    label[0] = '\0';
 
-	for(i=0; i<strlen(input); i++){
-		if(input[i] == '#') // Line is over if comment begins
-			break;
-		else if(input[i] == ':'){ // line label
-			add_label(&label[0], lineNumber);
-			label[0] = '\0';
+    for(i=0; i<strlen(input); i++){
+        if(input[i] == '#') // Line is over if comment begins
+            break;
+        else if(input[i] == ':'){ // line label
+            add_label(&label[0], lineNumber);
+            label[0] = '\0';
 
-		}
-		else if(input[i] != ' '){
-			int len = strlen(label);
-			label[len] = input[i];
-			label[len+1] = '\0';
-		}
-	}
+        }
+        else if(input[i] != ' '){
+            int len = strlen(label);
+            label[len] = input[i];
+            label[len+1] = '\0';
+        }
+    }
 
-	if(label[0] != '\0'){
-		lineNumber++;
-	}
-	return 0;
+    if(label[0] != '\0'){
+        lineNumber++;
+    }
+    return 0;
 }
 
 int parse_file(char* filename, int mode){
@@ -204,29 +204,29 @@ int parse_file(char* filename, int mode){
 
     char line[256] = "";
     while( ( ch = fgetc(readFile) ) != EOF ){
-    	if(ch == '\n'){
-    		// Parse line / label
+        if(ch == '\n'){
+            // Parse line / label
 
-    		if(mode == 0){
-    			parse_line_labels(line);
-    			line[0] = '\0'; // Empty array
-    		}
-    		else{
-    			parse_line(line);
-    			if(strcmp(lineWord, EMPTY_WORD)){
-    				printf("%s\n", lineWord);
-    				fprintf(writeFile,"%s\n", lineWord);
-    			}
+            if(mode == 0){
+                parse_line_labels(line);
+                line[0] = '\0'; // Empty array
+            }
+            else{
+                parse_line(line);
+                if(strcmp(lineWord, EMPTY_WORD)){
+                    printf("%s\n", lineWord);
+                    fprintf(writeFile,"%s\n", lineWord);
+                }
 
-    			line[0] = '\0'; // Empty array
-    		}
-		}
-    	else{
-    		// Append ch to the line
-			int len = strlen(line);
-			line[len] = ch;
-			line[len+1] = '\0';
-    	}
+                line[0] = '\0'; // Empty array
+            }
+        }
+        else{
+            // Append ch to the line
+            int len = strlen(line);
+            line[len] = ch;
+            line[len+1] = '\0';
+        }
     }
 
     fclose(writeFile);
@@ -238,9 +238,9 @@ int parse_file(char* filename, int mode){
 
 int main(int argc, char* argv[]){
 
-	parse_file(argv[1], 0); // parse for labels
-	init_dictionary();
-	parse_file(argv[1], 1); // parse for everything else
+    parse_file(argv[1], 0); // parse for labels
+    init_dictionary();
+    parse_file(argv[1], 1); // parse for everything else
 
     return 0;
 }
